f1c5d4e1ce5bbd097d0fde28b9370d943f79cd79
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-03-14 10:35:52 +0800

    (add) Refactored files

---
+++ feature_extraction.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 08:53:55 2023
+
+@author: Enzo
+"""
+from skimage.feature import graycomatrix, graycoprops
+from skimage.feature import local_binary_pattern
+
+import seaborn as sns
+sns.set_theme(style="ticks")
+
+from alive_progress import alive_bar
+import numpy as np
+
+def extract_lbp(images):
+    lbps = []
+    with alive_bar(len(images), bar='smooth', spinner=None) as bar:
+        for image in images:
+            lbp = local_binary_pattern(image, P=8, R=1)
+            lbps.append(lbp)
+            bar()
+
+    return np.array(lbps)
+
+def create_histograms(images, sub_images_num, bins_per_sub_images):
+    all_histograms = []
+
+    with alive_bar(len(images), bar='smooth', spinner=None) as bar:
+        for image in images:
+            grid = np.arange(
+                0, image.shape[1]+1, image.shape[1]//sub_images_num)
+
+            sub_image_histograms = []
+
+            for i in range(1, len(grid)):
+                for j in range(1, len(grid)):
+                    sub_image = image[grid[i-1]:grid[i], grid[j-1]:grid[j]]
+
+                    sub_image_histogram = np.histogram(
+                        sub_image, bins=bins_per_sub_images)[0]
+                    sub_image_histograms.append(sub_image_histogram)
+
+            histogram = np.array(sub_image_histograms).flatten()
+
+            all_histograms.append(histogram)
+            bar()
+
+    return np.array(all_histograms)
+
+def extract_glcm(images,
+                 sub_images_num,
+                 dists=[5],
+                 angles=[0, np.pi/4, np.pi/2, 3*np.pi/4],
+                 lvl=256,
+                 sym=True,
+                 norm=True):
+
+    props = ['dissimilarity', 'correlation',
+             'homogeneity', 'contrast', 'ASM', 'energy']
+    glcms = []
+    with alive_bar(len(images), bar='smooth', spinner=None) as bar:
+        for image in images:
+
+            grid = np.arange(
+                0, image.shape[1]+1, image.shape[1]//sub_images_num)
+
+            image_features = []
+            sub_image_features = []
+            for i in range(1, len(grid)):
+                for j in range(1, len(grid)):
+                    sub_image = image[grid[i-1]:grid[i], grid[j-1]:grid[j]]
+
+                    glcm = graycomatrix(sub_image,
+                                        distances=dists,
+                                        angles=angles,
+                                        levels=lvl,
+                                        symmetric=sym,
+                                        normed=norm)
+
+                    glcm_props = [prop for name in props for
+                                  prop in graycoprops(glcm, name)[0]]
+
+                    for item in glcm_props:
+                        sub_image_features.append(item)
+
+                    image_features.append(sub_image_features)
+
+            features = np.array(image_features).flatten()
+
+            glcms.append(features)
+            bar()
+
+    return np.array(glcms)
+
+def extract_glcm_noloop(images,
+                 dists=[5],
+                 angles=[0, np.pi/4, np.pi/2, 3*np.pi/4],
+                 lvl=256,
+                 sym=True,
+                 norm=True):
+
+    props = ['dissimilarity', 'correlation',
+             'homogeneity', 'contrast', 'ASM', 'energy']
+    
+    glcms = []
+    with alive_bar(len(images), bar='smooth', spinner=None) as bar:
+        for image in images:
+            
+            
+      
+            features = []
+            glcm = graycomatrix(image,
+                                distances=dists,
+                                angles=angles,
+                                levels=lvl,
+                                symmetric=sym,
+                                normed=norm)
+
+            glcm_props = [prop for name in props for
+                          prop in graycoprops(glcm, name)[0]]
+
+            for item in glcm_props:
+                features.append(item)
+
+            glcms.append(features)
+            bar()
+
+    return np.array(glcms)
---
+++ utils.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 10:44:33 2023
+
+@author: lorenzoquerol
+"""
+
+import seaborn as sns
+sns.set_theme(style="ticks")
+
+from PIL import Image
+import smartcrop
+import cv2
+
+from alive_progress import alive_bar
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (10, 10)
+
+def load_images(path):
+    images = []
+    filenames = os.listdir(path)
+
+    with alive_bar(len(filenames), bar='smooth', spinner=None) as bar:
+        for filename in filenames:
+            image = cv2.imread(os.path.join(path, filename))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            images.append(image)
+            bar()
+
+    return np.array(images, dtype=object)
+
+def show_raw_images(images, classname, start_index=0):
+    fig, axes = plt.subplots(ncols=5, nrows=2, figsize=(16, 2.5))
+    plt.suptitle(classname)
+    axes = axes.ravel()
+    index = start_index
+    for i in range(10):
+        axes[i].imshow(images[index])
+        axes[i].set_title(images[index].shape)
+        axes[i].get_xaxis().set_visible(False)
+        axes[i].get_yaxis().set_visible(False)
+        index += 1
+    plt.show()
+
+def crop_images(images, size):
+    sc = smartcrop.SmartCrop()
+    cropped_images = []
+    
+    with alive_bar(len(images), bar='smooth', spinner=None) as bar:
+        for image in images:
+            image = np.array(image)
+            image = Image.fromarray(image)
+            localized = sc.crop(image, size, size)
+        
+            bbox = (
+                localized['top_crop']['x'],
+                localized['top_crop']['y'],
+                localized['top_crop']['width'] + localized['top_crop']['x'],
+                localized['top_crop']['height'] + localized['top_crop']['y']
+            )
+        
+            cropped = image.crop(bbox)
+            resized = cropped.resize((size, size))
+            cropped_images.append(np.array(resized))
+            bar()
+    
+    return np.array(cropped_images)
+    
+def preprocess_images(images):
+    preprocessed_images = []
+    
+    with alive_bar(len(images), bar='smooth', spinner=None) as bar:
+        for image in images:
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            image = cv2.resize(image, dsize=(150, 150))
+            image = image / 255.
+            preprocessed_images.append(image)
+            bar()
+    
+    hsv_images = []
+    with alive_bar(len(images), bar='smooth', spinner=None) as bar:
+        for image in images:
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            image = cv2.resize(image, dsize=(150, 150))
+            image = image / 255.
+            hsv_images.append(image)
+            bar()
+            
+    return np.array(preprocessed_images), np.array(hsv_images)
+
+def find_misclassifications(labels, preds):
+    indices = []
+    for i, (label, pred) in enumerate(zip(preds, labels)):
+        if pred != label:
+            indices.append(i)
+
+    return np.array(indices)
+
+def show_misclassifications(images, misclassified, labels, preds, start_index=0):
+    fig, axes = plt.subplots(ncols=7, nrows=2, figsize=(18, 6))
+    
+    classnames = ['healthy', 'wssv']
+    index = start_index
+    for i in range(2):
+        for j in range(7):
+            axes[i, j].imshow(images[misclassified[index]], cmap='gray')
+            axes[i, j].set_title(f'actual: {classnames[labels[misclassified[index]]]} \n'
+                                 f'pred: {classnames[preds[misclassified[index]]]}')
+            axes[i, j].get_xaxis().set_visible(False)
+            axes[i, j].get_yaxis().set_visible(False)
+            if index == (index+14):
+                break
+            index += 1
+    plt.show()
+
+
+
