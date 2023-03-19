4a3535b1908eed595f3eb7cd099b4c7c965f5e7f
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-03-19 15:50:00 +0800

    (update) Removed file saving

--- data_augmentation.py
+++ data_augmentation.py
@@ -19,10 +19,6 @@ def augment_images(images, labels, num_aug=5):
             augmented = transform(image=image)["image"]
             augmented_images.append(augmented)
             augmented_labels.append(label)
-            
-            # filename = img_fn.replace('.jpg','') # Get image base filename
-            # new_filename = base_fn + ('_aug%d' % (i+1)) + IMG_EXTENSION # Append "aug#" to filename
-            # img_aug_bgr1 = cv2.cvtColor(img_aug1, cv2.COLOR_RGB2BGR) # Re-color to BGR from RGB\
     
     zipped = list(zip(augmented_images, augmented_labels))            
     new_dataset = np.vstack((images, zipped))
