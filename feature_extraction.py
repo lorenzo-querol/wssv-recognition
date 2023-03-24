2de27b9f2b1ef05bce0abfb63d4091c3a8eaf3c7
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-03-24 13:01:30 +0800

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -4,29 +4,60 @@ Created on Tue Mar 14 08:53:55 2023
 
 @author: Enzo
 """
+import matplotlib.pyplot as plt
+import numpy as np
+from alive_progress import alive_bar
 from skimage.feature import graycomatrix, graycoprops
 from skimage.feature import local_binary_pattern
 
 import seaborn as sns
 sns.set_theme(style="ticks")
 
-from alive_progress import alive_bar
-import numpy as np
+
+plt.rcParams['figure.dpi'] = 600
+
 
 def extract_lbp(images):
     lbps = []
-    with alive_bar(len(images), bar='smooth', spinner=None) as bar:
+    with alive_bar(len(images), title="Extracting LBP", bar='smooth', spinner=None) as bar:
         for image in images:
-            lbp = local_binary_pattern(image, P=8, R=1)
+            lbp = local_binary_pattern(image, P=8, R=2)
             lbps.append(lbp)
             bar()
 
     return np.array(lbps)
 
+
+def split_image(image, sub_images_num, bins_per_sub_images):
+
+    fig, ax = plt.subplots()
+
+    grid = np.arange(
+        0, image.shape[1]+1, image.shape[1]//sub_images_num)
+
+    sub_image_histograms = []
+
+    for i in range(1, len(grid)):
+        for j in range(1, len(grid)):
+            sub_image = image[grid[i-1]:grid[i], grid[j-1]:grid[j]]
+
+            sub_image_histogram = np.histogram(sub_image,
+                                               bins=bins_per_sub_images)[0]
+
+            sub_image_histograms.append(sub_image_histogram)
+
+    histogram = np.array(sub_image_histograms).flatten()
+    ax.hist(histogram)
+    plt.axis('off')
+    plt.show()
+
+
 def create_histograms(images, sub_images_num, bins_per_sub_images):
     all_histograms = []
 
-    with alive_bar(len(images), bar='smooth', spinner=None) as bar:
+    # fig, ax = plt.subplots(3, 3)
+
+    with alive_bar(len(images), title="Creating Histograms", bar='smooth', spinner=None) as bar:
         for image in images:
             grid = np.arange(
                 0, image.shape[1]+1, image.shape[1]//sub_images_num)
@@ -36,6 +67,8 @@ def create_histograms(images, sub_images_num, bins_per_sub_images):
             for i in range(1, len(grid)):
                 for j in range(1, len(grid)):
                     sub_image = image[grid[i-1]:grid[i], grid[j-1]:grid[j]]
+
+                    # ax[i-1, j-1].imshow(sub_image)
 
                     sub_image_histogram = np.histogram(
                         sub_image, bins=bins_per_sub_images)[0]
@@ -47,6 +80,7 @@ def create_histograms(images, sub_images_num, bins_per_sub_images):
             bar()
 
     return np.array(all_histograms)
+
 
 def extract_glcm(images,
                  sub_images_num,
@@ -61,7 +95,7 @@ def extract_glcm(images,
     glcms = []
     with alive_bar(len(images), bar='smooth', spinner=None) as bar:
         for image in images:
-            
+
             grid = np.arange(
                 0, image.shape[1]+1, image.shape[1]//sub_images_num)
 
@@ -93,37 +127,38 @@ def extract_glcm(images,
 
     return np.array(glcms)
 
+
 def extract_glcm_noloop(images,
-                 dists=[5],
-                 angles=[0, np.pi/4, np.pi/2, 3*np.pi/4],
-                 lvl=256,
-                 sym=True,
-                 norm=True):
+                        dists=[5],
+                        angles=[0, np.pi/4, np.pi/2, 3*np.pi/4],
+                        lvl=256,
+                        sym=True,
+                        norm=True):
 
     props = ['dissimilarity', 'correlation',
              'homogeneity', 'contrast', 'ASM', 'energy']
-    
+
     glcms = []
     with alive_bar(len(images), bar='smooth', spinner=None) as bar:
         for image in images:
             channel_features = []
             for c in range(3):
                 features = []
-                glcm = graycomatrix(image[:,:,c],
+                glcm = graycomatrix(image[:, :, c],
                                     distances=dists,
                                     angles=angles,
                                     levels=lvl,
                                     symmetric=sym,
                                     normed=norm)
-        
+
                 glcm_props = [prop for name in props for
                               prop in graycoprops(glcm, name)[0]]
-        
+
                 for item in glcm_props:
                     features.append(item)
-                
+
                 channel_features.append(features)
-                
+
             glcms.append(channel_features)
             bar()
 
