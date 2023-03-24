05e4f8dcd589a169ba817ea819443198e96de0f5
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-03-24 13:01:28 +0800

    Update data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import albumentations as A
+from alive_progress import alive_bar
 import numpy as np
 
 transform = A.Compose([
@@ -10,19 +11,22 @@ transform = A.Compose([
     A.GaussianBlur()
 ])
 
+
 def augment_images(images, labels, num_aug=5):
     augmented_images = []
     augmented_labels = []
-    
-    for image, label in images:
-        for i in range(num_aug):
-            augmented = transform(image=image)["image"]
-            augmented_images.append(augmented)
-            augmented_labels.append(label)
-    
-    zipped = list(zip(augmented_images, augmented_labels))            
+
+    with alive_bar(len(images), title="Augmenting Images", bar='smooth', spinner=None) as bar:
+        for image, label in images:
+            for i in range(num_aug):
+                augmented = transform(image=image)["image"]
+                augmented_images.append(augmented)
+                augmented_labels.append(label)
+            bar()
+
+    zipped = list(zip(augmented_images, augmented_labels))
     new_dataset = np.vstack((images, zipped))
-    
+
     new_labels = np.hstack((labels, augmented_labels))
-    
+
     return new_dataset, new_labels
