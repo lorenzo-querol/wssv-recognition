de9c9f1fd7b03208b593f27c2bf1bfd18ccd24de
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-03-29 19:27:22 +0800

    [Transfer] from macbook

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Mon Mar 20 18:53:54 2023
 
@@ -6,7 +6,7 @@ Created on Mon Mar 20 18:53:54 2023
 """
 # %%
 
-from ../functions/data_augmentation import augment_images
+from data_augmentation import augment_images
 from feature_extraction import extract_lbp, create_histograms, extract_glcm_noloop, split_image
 from utils import load_images, show_raw_images, crop_images, preprocess_images
 
@@ -24,7 +24,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
-from sklearn.feature_selection import SelectKBest, f_classif, chi2, RFECV
+from sklearn.feature_selection import SelectKBest, f_classif, chi2, RFE
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, cross_validate, train_test_split
 
 from imblearn.combine import SMOTETomek
@@ -33,7 +33,7 @@ from imblearn.under_sampling import TomekLinks
 from imblearn.pipeline import Pipeline
 
 sns.set_theme(style="ticks")
-plt.rcParams['figure.dpi'] = 100
+plt.rcParams['figure.dpi'] = 150
 random_state = 42
 
 # %% Data Loading
@@ -91,14 +91,14 @@ x_test_lbp_hist = create_histograms(x_test_lbp,
                                     sub_images_num=3,
                                     bins_per_sub_images=64)
 
-# %% Helper function
+#%% Helper functions
 
 
-def evaluate(pipeline):
+def train_validate(pipeline):
     cv = StratifiedKFold(n_splits=10,
                          shuffle=True,
                          random_state=random_state)
-
+    
     scoring = ['accuracy', 'precision_macro', 'recall_macro', 'f1_weighted']
 
     scores = cross_validate(pipeline,
@@ -109,7 +109,7 @@ def evaluate(pipeline):
                             n_jobs=-1,
                             return_train_score=True,
                             return_estimator=True)
-
+    
     print('\nTraining')
     print('Accuracy: %.4f' % max(scores['train_accuracy']))
     print('Precision: %.4f' % max(scores['train_precision_macro']))
@@ -121,41 +121,21 @@ def evaluate(pipeline):
     print('Precision: %.4f' % max(scores['test_precision_macro']))
     print('Recall: %.4f' % max(scores['test_recall_macro']))
     print('F1 Score: %.4f' % max(scores['test_f1_weighted']))
-
-    pipeline.fit(x_train_lbp_hist, y_train)
-    y_preds_svm = pipeline.predict(x_test_lbp_hist)
-
-    test_acc = pipeline.score(x_test_lbp_hist, y_test)
-    test_f1 = f1_score(y_test, y_preds_svm)
-
-    cm = confusion_matrix(y_test, y_preds_svm, labels=[0, 1])
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
-                                  display_labels=classnames)
-
-    FN = cm.sum(axis=1) - np.diag(cm)
-    TP = np.diag(cm)
-    FNR = FN / (TP + FN)
-
-    print('\nTest')
-    print('Accuracy: %.4f' % test_acc)
-    print('F1 score: %.4f' % test_f1)
-    print('False Negative Rate (Healthy): %.4f' % FNR[0])
-    print('False Negative Rate (WSSV): %.4f' % FNR[1])
-
-    disp.plot()
-    plt.show()
+    
+def evaluate(pipeline):
+    pass
 
 # %% Classification
 
-
 scaler = StandardScaler()
-
 classifier1 = SVC(random_state=random_state)
+
 pipeline1 = Pipeline(steps=[('scaler', scaler),
                             ('classifier', classifier1)
                             ])
 
 classifier2 = SVC(random_state=random_state)
+
 sampler = SMOTETomek(tomek=TomekLinks(sampling_strategy='majority'),
                      smote=SMOTE(sampling_strategy='minority')
                      )
@@ -166,7 +146,31 @@ pipeline2 = Pipeline(steps=[('scaler', scaler),
                             ])
 
 print("\nWithout SMOTE and Tomek-Links")
-evaluate(pipeline1)
+train_validate(pipeline1)
 
 print("\nAfter Applying SMOTE and Tomek-Links")
-evaluate(pipeline2)
+train_validate(pipeline2)
+
+
+# pipeline.fit(x_train_lbp_hist, y_train)
+# y_preds_svm = pipeline.predict(x_test_lbp_hist)
+
+# test_acc = pipeline.score(x_test_lbp_hist, y_test)
+# test_f1 = f1_score(y_test, y_preds_svm)
+
+# cm = confusion_matrix(y_test, y_preds_svm, labels=[0, 1])
+# disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+#                               display_labels=classnames)
+
+# FN = cm.sum(axis=1) - np.diag(cm)
+# TP = np.diag(cm)
+# FNR = FN / (TP + FN)
+
+# print('\nTest')
+# print('Accuracy: %.4f' % test_acc)
+# print('F1 score: %.4f' % test_f1)
+# print('False Negative Rate (Healthy): %.4f' % FNR[0])
+# print('False Negative Rate (WSSV): %.4f' % FNR[1])
+
+# disp.plot()
+# plt.show()
