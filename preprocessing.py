58de1b6122b489fddac874da683906eb723fd2da
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-05-17 22:53:59 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -4,13 +4,16 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def kmeans_segmentation(image):
     # Reshape the image to a 2D array of pixels
     pixels = image.reshape(-1, 3).astype(np.float32)
 
     # Define the criteria and perform k-means clustering
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    _, labels, centers = cv.kmeans(pixels, 2, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+    _, labels, centers = cv.kmeans(
+        pixels, 2, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS
+    )
 
     # Calculate the average pixel value for each cluster
     average_colors = np.uint8(centers)
@@ -29,11 +32,12 @@ def kmeans_segmentation(image):
     # Invert the segmentation mask if the white area is larger than the segmented part
     if white_area > (labels.size - white_area):
         labels = 1 - labels
-        
+
     # Segment the image based on the labels
     segmented_image = np.where(labels == 1, 255, 0).astype(np.uint8)
 
     return segmented_image
+
 
 def spot_cleanup(image):
     # Convert the image to grayscale
@@ -44,11 +48,15 @@ def spot_cleanup(image):
 
     # Perform morphological operations to clean the spots in the background
     kernel_background = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
-    cleaned_background = cv.morphologyEx(binary, cv.MORPH_OPEN, kernel_background, iterations=2)
+    cleaned_background = cv.morphologyEx(
+        binary, cv.MORPH_OPEN, kernel_background, iterations=2
+    )
 
     # Perform morphological operations to close the spots in the foreground
     kernel_foreground = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
-    closed_foreground = cv.morphologyEx(cleaned_background, cv.MORPH_CLOSE, kernel_foreground, iterations=2)
+    closed_foreground = cv.morphologyEx(
+        cleaned_background, cv.MORPH_CLOSE, kernel_foreground, iterations=2
+    )
 
     # Invert the binary image to obtain the final mask
     mask = cv.bitwise_not(closed_foreground)
@@ -58,11 +66,12 @@ def spot_cleanup(image):
 
     return segmented_image
 
+
 def preprocess_image(image, label):
     # Perform spot cleanup
     image = np.array(image)
     preprocessed_image = kmeans_segmentation(image)
     preprocessed_image = spot_cleanup(preprocessed_image)
     preprocessed_image = preprocessed_image.convert_to_tensor()
-    
+
     return preprocessed_image, label
